e1cf6a3a4a9540611da44007f9918466396567dd
6p00l <nolf.91@gmail.com>
2022-04-29 01:16:48 +0300

    add: remove-from-list solution

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -22,11 +22,45 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(list, k) {
+  let temp;
+  if (!list.value) {
+    return null;
+  }
+  let length = 0;
+  while (list.value === k) {
+    list.value = list.next.value;
+    list.next = list.next.next;
+    length++;
+  }
+  let current = list.next;
+  let prev = list;
+  function da(index) {
+    for (let i = 0; i < index; i++) {
+      prev = prev.next;
+      return prev;
+    }
+  }
+  while (current != null) {
+    length++;
+
+    if (current.value === k) {
+      if (current.next != null) {
+        temp = current;
+        current.value = current.next.value;
+        current.next = current.next.next;
+        current = temp;
+        length--;
+      } else {
+        da(length);
+        prev.next = null;
+        break;
+      }
+    } else current = current.next;
+  }
+  return list;
 }
 
 module.exports = {
-  removeKFromList
+  removeKFromList,
 };
