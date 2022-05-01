14ddb131cdeab9958e65e350bb141755f1dbf122
6p00l <nolf.91@gmail.com>
2022-05-01 23:37:04 +0300

    feat: add bst solution

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -57,24 +57,82 @@ class BinarySearchTree {
     }
   }
 
-  find(data) {}
+  find(data) {
+    let node = this.rootNode;
 
-  remove(/* data */) {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
-  }
-
-  min() {
-    throw new NotImplementedError("Not implemented");
-    // remove line with error and write your code here
-  }
-
-  max() {
-    let root = this.rootNode;
-    while (root.right != null) {
-      root = root.right;
+    if (node.data === data) {
+      return node;
     }
-    return root.data;
+
+    while (true) {
+      if (node.data === data) {
+        return node;
+      }
+      if (data > node.data) {
+        if (node.right != null) {
+          node = node.right;
+          continue;
+        } else return null;
+      }
+      if (data < node.data) {
+        if (node.left != null) {
+          node = node.left;
+          continue;
+        } else return null;
+      }
+    }
+  }
+
+  remove(value) {
+    if (this.rootNode === null) {
+      return null;
+    }
+
+    let delnode = this.find(value);
+    if (delnode === null) {
+      return;
+    }
+    let prev = this.prev(value);
+    function del() {
+      if (value < prev.data) {
+        prev.left = null;
+      } else prev.right = null;
+    }
+
+    while (this.has(value)) {
+      if (delnode.left === null || delnode.right === null) {
+        if (delnode.left === null && delnode.right === null) {
+          del();
+          continue;
+        } else {
+          if (delnode.left === null) {
+            if (value < prev.data) {
+              prev.left = delnode.right;
+            } else prev.right = delnode.right;
+          }
+          if (delnode.right === null) {
+            if (value < prev.data) {
+              prev.left = delnode.left;
+            } else prev.right = delnode.left;
+          }
+        }
+        continue;
+      }
+      let last = this.find(this.min(delnode.right));
+      delnode.data = last.data;
+    }
+  }
+
+  min(node = this.rootNode) {
+    if (node.left === null) return node.data;
+    else return this.min(node.left);
+  }
+
+  max(node = this.rootNode) {
+    while (node.right != null) {
+      node = node.right;
+    }
+    return node.data;
   }
 
   deep(node, newNode) {
@@ -94,6 +152,24 @@ class BinarySearchTree {
     if (newNode.data < node.data) {
       this.deep(node.left, newNode);
     } else this.deep(node.right, newNode);
+  }
+
+  prev(value) {
+    let node = this.rootNode;
+    let prev = node;
+    while (true) {
+      if (node.data === value) {
+        return prev;
+      } else prev = node;
+      if (value < node.data) {
+        node = node.left;
+        continue;
+      }
+      if (value > node.data) {
+        node = node.right;
+        continue;
+      }
+    }
   }
 }
 
